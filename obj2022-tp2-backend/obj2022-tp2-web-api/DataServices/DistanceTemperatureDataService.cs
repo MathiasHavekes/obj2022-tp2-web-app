ea573b29dcbbc9e1e61e306a5967c465bb7d5b11
Xavier Paquet-Rapold <paquetrapoldx@protonmail.com>
2022-05-15 11:09:41 -0700

    Make the chart work

--- obj2022-tp2-backend/obj2022-tp2-web-api/DataServices/DistanceTemperatureDataService.cs
+++ obj2022-tp2-backend/obj2022-tp2-web-api/DataServices/DistanceTemperatureDataService.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using obj2022_tp2_web_api.Models;
+using System.Collections.Generic;
 
 namespace obj2022_tp2_web_api.DataServices
 {
@@ -24,6 +25,28 @@ namespace obj2022_tp2_web_api.DataServices
                 }
 
                 return distanceTemperature.First();
+            }
+        }
+
+        public async Task<IEnumerable<DistanceTemperature>> GetAllDistanceTemperatureAsync()
+        {
+            var path = "https://obj2022-iot-http-trigger20220511163325.azurewebsites.net/api/GetAllDistanceTemperature?code=8u7D40vuD_sFShoCyjhOyRunuNVJtBhkP1J_iq7KAKcWAzFuDh8wiQ==";
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(path);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new List<DistanceTemperature>();
+                }
+                var responseString = await response.Content.ReadAsStringAsync();
+                var distanceTemperature = JsonConvert.DeserializeObject<List<DistanceTemperature>>(responseString);
+
+                if (distanceTemperature == null)
+                {
+                    return new List<DistanceTemperature>();
+                }
+
+                return distanceTemperature;
             }
         }
     }
