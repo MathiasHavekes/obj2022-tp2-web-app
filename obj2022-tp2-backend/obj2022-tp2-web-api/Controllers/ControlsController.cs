1f5074a87ef82d5e0c18cb1213b1871e03d1eb9d
Xavier Paquet-Rapold <paquetrapoldx@protonmail.com>
2022-05-15 14:34:39 -0700

    Send service messages to device

--- obj2022-tp2-backend/obj2022-tp2-web-api/Controllers/ControlsController.cs
+++ obj2022-tp2-backend/obj2022-tp2-web-api/Controllers/ControlsController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using obj2022_tp2_web_api.DataServices;
+using Microsoft.Azure.Devices;
+using Newtonsoft.Json;
 using obj2022_tp2_web_api.Models.Dtos;
+using System.Text;
 
 namespace obj2022_tp2_web_api.Controllers
 {
@@ -8,16 +10,30 @@ namespace obj2022_tp2_web_api.Controllers
     [Route("tomato")]
     public class ControlsController : ControllerBase
     {
-        public ControlsController()
-        {
-
-        }
+        private const string IotHubUri = "iotObjectHub.azure-devices.net";
+        private const string DeviceKey = "KyNxXceKfSnO/wiQM9hG8mzEjikB4Ytb3/MvvGhxn6Q=";
+        private const string ConnectionString = "HostName=iotObjectHub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=nECnOMEb2L1BDBN4owfKN3Y/NXG9VbJrxtHVho4bz7Y=";
+        private const string DeviceId = "tempsensor";
 
         [HttpPost("controls")]
         public async Task<bool> PostControlState(
             [FromBody] ControlDto newControls
         )
         {
+            using (var client = ServiceClient.CreateFromConnectionString(ConnectionString))
+            {
+                var control = new
+                {
+                    deviceId = DeviceId,
+                    state = newControls.State.ToString(),
+                    target = newControls.target
+                };
+                var messageString = JsonConvert.SerializeObject(control);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                message.Properties.Add("Topic", "WaterUsage");
+
+                await client.SendAsync(DeviceId, message);
+            }
             return true;
         }
     }
